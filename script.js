d8e437579031821449d3bf048a8564d32554307f
Andrew-devcoder <andrey.kovpak.01@gmail.com>
2023-07-31 21:52:26 +0300

    burger menu draft

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 const burgerButton = document.querySelector(".menu-burger");
-console.log(burgerButton);
+const burgerMenuList = document.querySelector(".menu-burger__list");
+
+burgerButton.addEventListener("click", () => {
+    burgerMenuList.classList.toggle("menu-burger__list--active");
+});
